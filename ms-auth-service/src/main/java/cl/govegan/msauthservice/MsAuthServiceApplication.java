d313d1f3a8a2b65fa8d0f0fa70bdee30fed7e1de
Gab-Serrano <gab.serrano@duocuc.cl>
2024-07-09 23:30:36 -0400

    CORS

--- ms-auth-service/src/main/java/cl/govegan/msauthservice/MsAuthServiceApplication.java
+++ ms-auth-service/src/main/java/cl/govegan/msauthservice/MsAuthServiceApplication.java
@@ -2,12 +2,30 @@ package cl.govegan.msauthservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.lang.NonNull;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MsAuthServiceApplication {
 
     public static void main (String[] args) {
         SpringApplication.run(MsAuthServiceApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:8100", "http://localhost:8080")
+                    .allowedMethods("GET", "POST", "PUT", "DELETE")
+                    .allowedHeaders("*")
+                    .allowCredentials(false);
+            }
+        };
     }
 
 }
